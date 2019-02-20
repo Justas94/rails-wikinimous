847e3b4ac56e3c91950cfe215b57005c2eed42d9
Justas Jaskunas <jaskunas.justas@gmail.com>
2019-02-20 10:48:26 +0000

    articles

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require "faker"
+
+10.times do
+  articles = Article.new(
+    title: Faker::Commerce.product_title,
+    content: Faker::Lorem.content
+  )
+  post.save
+end
